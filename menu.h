191ed1da1b8f46fb3e5cd29cb2069d6907051dc2
AmruthaVarshini555 <119595744+AmruthaVarshini555@users.noreply.github.com>
2023-01-15 11:06:25 +0530

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+#include"logger.h"
+using namespace std;
+
+class Menu
+{
+    public:
+        Menu(){
+            LOG_INFO("\nDefault constructor of mainmenu invoked");
+        }
+        void welcomeScreen();
+        void mainMenu();
+        void adminLogin();
+        void adminMenu();
+        void userLogin();
+        void userMenu();
+        void viewBookingsMenu();
+        ~Menu(){
+            LOG_INFO("\nDestructor invoked");
+        }
+};
