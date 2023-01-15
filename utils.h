e5825ee5d766fa75ef64ba26edb1f50671a75f66
AmruthaVarshini555 <119595744+AmruthaVarshini555@users.noreply.github.com>
2023-01-15 11:00:35 +0530

    Update utils.h

--- utils.h
+++ utils.h
@@ -6,25 +6,12 @@
 #include <ctime>
 
 using namespace std;
-
-void printHeading(string header)
-{
-    cout << "\n\n\n\n";
-    cout << "\t\t\t\t\t\t\t\t\t\t==========================================================\n";
-    cout << "\t\t\t\t\t\t\t\t\t\t                       " << header << "                     \n";
-    cout << "\t\t\t\t\t\t\t\t\t\t==========================================================\n\n";
-}
-
 string generatePNR(int n)
 {
     srand(time(0));
-
     string pnr;
-
     int randomNo = rand() % n;
-
     pnr = "PNR" + to_string(randomNo);
-
     return pnr;
 }
 
@@ -32,7 +19,6 @@ string getCurrentDate()
 {
     time_t t = time(NULL);
     tm *tPtr = localtime(&t);
-
     return to_string(tPtr->tm_mday) + "-" + to_string((tPtr->tm_mon) + 1) + "-" + to_string((tPtr->tm_year) + 1900);
 }
 #endif // UTILS_H
